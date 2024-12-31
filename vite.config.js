80a8d4e42de01756eddb88328b70fcccc667c3b7
AnkitKumarHub <akkimessi710@gmail.com>
2024-12-31 19:36:21 +0530

    Port defined

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Or any port you prefer
+    host: true, // Ensures it binds to 0.0.0.0
+  },
 })
+
+
